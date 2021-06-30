41ecf0c17fe9f1237e455ed0153b8dd82bcad9d9
nguyenvandung234 <86741032+nguyenvandung234@users.noreply.github.com>
2021-07-01 01:39:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương
-    Mắc cười hong`,
+    Mắc cười hong `,
     btnIntro: 'Bé ơi',
     title: 'Bé ơi bé có yêu anh nhìu hong 😙',
     desc: 'Phải nói thiệt nhe ',
-    btnYes: 'Vẫn yêu anh nhìu lém',
+    btnYes: 'Vẫn yêu anh nhìu lém ',
     btnNo: 'Không, hết iu rui ',
-    question:'Trên thế giới có 1 lon người mà sao em lại yêu anh dạ',
-    btnReply: 'Gửi cho chồng péo',
+    question:'Trên thế giới có 1 lon người mà sao em lại yêu anh dạ ',
+    btnReply: 'Gửi cho chồng péo ',
     reply: 'Em có yêu anh mà, yêu nhìu lun hehe ',
-    mess: 'Anh biết mà 🥰. Yêu Tú nhiềuuuuuuuu 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu Tú nhiềuuuuuuuu 😘😘 ',
     messDesc: 'Em đừng giận anh nữa nha cục cưng.',
-    btnAccept: 'Hết giận nhe. Mặc dù hơi sến mà thui đừng giận anh nữa nhe',
+    btnAccept: 'Hết giận nhe. Mặc dù hơi sến mà thui đừng giận anh nữa nhe ',
 }
