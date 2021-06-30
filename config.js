64b93f69b8d49fa30fa85a84d1de5cc57ac7d08a
nguyenvandung234 <86741032+nguyenvandung234@users.noreply.github.com>
2021-07-01 01:23:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Come To à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,11 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 1 lon người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Yêu thì yêu mà không yêu thì yêu ',
+    mess: 'Anh biết mà 🥰. Yêu Tú nhiều nhiều mà huhu 😘😘',
+    messDesc: 'Em đừng giận anh nữa nha cục cưng.',
+    btnAccept: 'Hết giận nhe. Mặc dù hơi sến mà thui đừng giận anh nữa nhe',
 }
